3267c2fab2d9810a1d47053844d46fec7a9e2c7f
iefnaug <guanfei1993@gmail.com>
2026-01-02 18:09:05 +0800

    init

---
+++ 03-变量.py
@@ -0,0 +1,55 @@
+if __name__ == '__main__':
+    # 整型
+    print(0b11)
+    print(0o11)
+    print(11)
+    print(0x11)
+
+    #浮点型
+    print(12.23)
+    print(1.22e6)
+
+    #字符串
+    print("hello python")
+
+    #bool
+    print(True)
+    print(False)
+
+    #类型
+    a = 10
+    b = 20.2
+    c = "hello python"
+    print(type(a))
+    print(type(b))
+    print(type(c))
+
+    #类型转换
+    a = 100
+    b = 123.45
+    c = '123'
+    d = '100'
+    e = '123.45'
+    f = 'hello, world'
+    g = True
+    print(float(a))  # int类型的100转成float，输出100.0
+    print(int(b))  # float类型的123.45转成int，输出123
+    print(int(c))  # str类型的'123'转成int，输出123
+    print(int(c, base=16))  # str类型的'123'按十六进制转成int，输出291
+    print(int(d, base=2))  # str类型的'100'按二进制转成int，输出4
+    print(float(e))  # str类型的'123.45'转成float，输出123.45
+    print(bool(f))  # str类型的'hello, world'转成bool，输出True
+    print(int(g))  # bool类型的True转成int，输出1
+    print(chr(a))  # int类型的100转成str，输出'd'
+    print(ord('d'))  # str类型的'd'转成int，输出100
+    print(int(True))
+    print(int(False))
+
+    #运算
+    a = 10
+    b = 3
+    a += b  # 相当于：a = a + b
+    a *= a + 2  # 相当于：a = a * (a + 2)
+    print(a)  # 大家算一下这里会输出什么
+
+    print((a := 10))
